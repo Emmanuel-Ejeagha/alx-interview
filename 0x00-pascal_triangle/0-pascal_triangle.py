6b23e35793663ddaf3ef05e38710ffcdf075d52d
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-01 22:55:18 +0100

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-This module implements the pascals triangle
+A module that implement the pascals triangle
 """
 
 
 def pascal_triangle(n):
 
     """
-    Returns a pascal triangle  , using list
+    The pascal triangle implementation , using list
     """
 
     if n <= 0:
         return []
     triangle = [[1]]
     for id in range(n - 1):
-        row = triangle[-1]
-        list_sqr = [1]
-        for idx, current in enumerate(row):
+        dat = triangle[-1]
+        lit_sqr = [1]
+        for idx, current in enumerate(dat):
             try:
-                next = row[idx + 1]
-                list_sqr.append(current + next)
+                next = dat[idx + 1]
+                lit_sqr.append(current + next)
             except Exception:
-                list_sqr.append(1)
-        triangle.append(list_sqr)
+                lit_sqr.append(1)
+        triangle.append(lit_sqr)
     return triangle
