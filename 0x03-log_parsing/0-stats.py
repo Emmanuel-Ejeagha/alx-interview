ae7b0f401e821b059c0d17388c3530cbd8ce8f73
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-06-20 22:17:03 +0100

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python3
-
-"""Module that reads stdin line by line and computes metrics
-
-
-"""
+"""Log Parser"""
 import sys
 
 
-def print_statistics(size, status):
-    """Prints the statistics based on the current state"""
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
-    print("File size: {:d}".format(size))
-    for k, v in sorted(status.items()):
-        if v > 0:
-            print("{}: {:d}".format(k, v))
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
-def my_main():
-    """The function that will solve the problem"""
-
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-    size = 0
-    count = 0
-    string = ""
+    linenum = 1
     try:
         for line in sys.stdin:
-            line = line.strip()
-            line_split = line.split()
-            try:
-                file_size = int(line_split[-1])
-                status_code = line_split[-2]
-                if status_code in status:
-                    status[status_code] += 1
-                else:
-                    raise Exception
-            except ValueError:
-                continue
-            except Exception:
-                size += file_size
-                continue
-            size += file_size
-            count += 1
-            if count % 10 == 0:
-                print_statistics(size, status)
-    finally:
-        print_statistics(size, status)
-
-
-if __name__ == '__main__':
-    my_main()
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
