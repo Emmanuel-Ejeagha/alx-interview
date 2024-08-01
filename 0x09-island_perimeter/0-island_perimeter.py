4caf0accb779f956da254242d5da9c95729dd1e5
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-08-01 16:21:26 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-# Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+
+""" Function to find perimiter of an island """
+
 
 def island_perimeter(grid):
-    """Calculate the perimeter of an highland"""
-    queue = []
-    perimeter = 0
-    n = len(grid)
-    m = len(grid[0])
-    for row in range(n):
-        for col in range(m):
-            if grid[row][col] == 1:
-                queue.append((row, col))
-                grid[row][col] = -2
-                break
-            
-            while queue:
-                try:
-                    row, col =queue.pop(0)
-                except IndexError:
-                    return perimeter
-                indexes = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-                for i, j in indexes:
-                    r = row + i
-                    c = col + j
-                    if r >= n or c >= m or r < 0 or c < 0 or grid[r][c] == 0:
-                        perimeter += 1
-                    elif grid[r][c] == 1:
-                        queue.append((r, c))
-                        grid[r][c] = -2
-            return perimeter
+    """
+    Input: List of Lists
+    Returns: Perimeter of the island
+    """
+    count = 0
+    row = len(grid)
+    col = len(grid[0]) if row else 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
+                     for k in idx]
+
+            if grid[i][j]:
+                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                              for r, k in zip(check, idx)])
+
+    return (count)
